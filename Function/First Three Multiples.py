4377bb228d0d591d959789b75fe41d2ca3b5b801
tuthvn <tuth.vn@gmail.com>
2019-04-06 05:18:05 +0700

    2

---
+++ Function/First Three Multiples.py
@@ -0,0 +1,11 @@
+# Write your first_three_multiples function here:
+def first_three_multiples(num):
+  for n in range(1,3):
+  	return num*3
+# Uncomment these function calls to test your first_three_multiples function:
+#first_three_multiples(10)
+print(first_three_multiples(10))
+# should print 10, 20, 30, and return 30
+#first_three_multiples(0)
+print(first_three_multiples(0))
+# should print 0, 0, 0, and return 0
